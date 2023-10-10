6d516ecf451c51d19f2a5598990f66c443108eb4
Mikaela-D <G00401392@atu.ie>
2023-10-10 23:25:46 +0100

    Added code to LibraryService

--- src/main/java/atu/ie/lab2_using_ioc_and_di/LibrarySystem/LibraryService.java
+++ src/main/java/atu/ie/lab2_using_ioc_and_di/LibrarySystem/LibraryService.java
@@ -1,4 +1,18 @@
 package atu.ie.lab2_using_ioc_and_di.LibrarySystem;
 
+import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class LibraryService {
+    private List<Book> books = new ArrayList<>();
+
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
 }
