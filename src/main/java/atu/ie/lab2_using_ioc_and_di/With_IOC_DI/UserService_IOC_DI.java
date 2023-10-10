a4cc701036c7b08360fde8c3d905478dc0c41c71
Mikaela-D <G00401392@atu.ie>
2023-10-10 23:00:40 +0100

    Added code to UserService_IOC_DI

--- src/main/java/atu/ie/lab2_using_ioc_and_di/With_IOC_DI/UserService_IOC_DI.java
+++ src/main/java/atu/ie/lab2_using_ioc_and_di/With_IOC_DI/UserService_IOC_DI.java
@@ -1,4 +1,21 @@
 package atu.ie.lab2_using_ioc_and_di.With_IOC_DI;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+// UserService with annotations
+@Service
 public class UserService_IOC_DI {
+    private final EmailService_IOC_DI emailService;
+
+    @Autowired      // Constructor injection
+    public UserService_IOC_DI(EmailService_IOC_DI emailService){
+        this.emailService = emailService;   // Loose Coupling
+
+    }
+    public void registerUser(String name, String email){
+        emailService.sendEmail(email, "Welcome to our platform");
+
+    }
+
 }
