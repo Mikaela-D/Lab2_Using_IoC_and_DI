1d9d84df055be61d405c628f19e36780ccd4d27a
Mikaela-D <G00401392@atu.ie>
2023-10-10 23:16:02 +0100

    Added code to UserController

--- src/main/java/atu/ie/lab2_using_ioc_and_di/RequestBody/UserController.java
+++ src/main/java/atu/ie/lab2_using_ioc_and_di/RequestBody/UserController.java
@@ -1,8 +1,21 @@
 package atu.ie.lab2_using_ioc_and_di.RequestBody;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+    private final RegisterUserBody registerUserBody;
 
+    @Autowired
+    public UserController(RegisterUserBody registerUserBody) {
+        this.registerUserBody = registerUserBody;
+    }
+
+    @PostMapping("/register")
+    public String Register(@RequestBody UserDetails userDetails){
+        return registerUserBody.msg(userDetails);
+    }
 }
