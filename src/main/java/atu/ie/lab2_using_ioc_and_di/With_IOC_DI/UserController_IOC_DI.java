a730a4c7090ba8c7fb7503d8113e34b81a727654
Mikaela-D <G00401392@atu.ie>
2023-10-10 23:03:26 +0100

    Added @RestController code to UserController_IOC_DI

--- src/main/java/atu/ie/lab2_using_ioc_and_di/With_IOC_DI/UserController_IOC_DI.java
+++ src/main/java/atu/ie/lab2_using_ioc_and_di/With_IOC_DI/UserController_IOC_DI.java
@@ -1,4 +1,15 @@
 package atu.ie.lab2_using_ioc_and_di.With_IOC_DI;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController_IOC_DI {
+    private final UserService_IOC_DI userService;
+
+    @Autowired
+    public UserController_IOC_DI(UserService_IOC_DI userService) {
+        this.userService = userService;
+    }
+
 }
