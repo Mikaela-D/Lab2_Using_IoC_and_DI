ecbe93bb6314f9f705063bbd49a6010ae2358c92
Mikaela-D <G00401392@atu.ie>
2023-10-10 22:48:43 +0100

    Added @RestController code to UserController

--- src/main/java/atu/ie/lab2_using_ioc_and_di/Without_IOC_DI/UserController.java
+++ src/main/java/atu/ie/lab2_using_ioc_and_di/Without_IOC_DI/UserController.java
@@ -1,4 +1,12 @@
 package atu.ie.lab2_using_ioc_and_di.Without_IOC_DI;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+    private UserService user;
+
+    public UserController(){
+        user = new UserService();
+    }
 }
