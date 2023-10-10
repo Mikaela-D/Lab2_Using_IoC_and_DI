f3d1575b0f56637938a23fb138384ea6c553a498
Mikaela-D <G00401392@atu.ie>
2023-10-10 22:55:59 +0100

    Added new classes EmailService_IOC_DI and UserController_IOC_DI

---
+++ src/main/java/atu/ie/lab2_using_ioc_and_di/With_IOC_DI/EmailService_IOC_DI.java
@@ -0,0 +1,4 @@
+package atu.ie.lab2_using_ioc_and_di.With_IOC_DI;
+
+public class EmailService_IOC_DI {
+}
---
+++ src/main/java/atu/ie/lab2_using_ioc_and_di/With_IOC_DI/UserController_IOC_DI.java
@@ -0,0 +1,4 @@
+package atu.ie.lab2_using_ioc_and_di.With_IOC_DI;
+
+public class UserController_IOC_DI {
+}
