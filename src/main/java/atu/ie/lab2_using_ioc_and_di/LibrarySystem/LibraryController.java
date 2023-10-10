e1b51a353ae0f72e3b975f6aa03fea8dcc7f185f
Mikaela-D <G00401392@atu.ie>
2023-10-10 23:29:50 +0100

    Added code to LibraryController

--- src/main/java/atu/ie/lab2_using_ioc_and_di/LibrarySystem/LibraryController.java
+++ src/main/java/atu/ie/lab2_using_ioc_and_di/LibrarySystem/LibraryController.java
@@ -1,4 +1,28 @@
 package atu.ie.lab2_using_ioc_and_di.LibrarySystem;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 public class LibraryController {
+    @RestController
+    @RequestMapping("/library")
+    public class LibraryController {
+
+        private LibraryService libraryService;
+
+        @Autowired
+        public void setLibraryService(LibraryService libraryService) {
+            this.libraryService = libraryService;
+        }
+
+        @PostMapping("/addBook")
+        public ResponseEntity<String> addBook(@RequestBody Book book) {
+            libraryService.addBook(book);
+            return ResponseEntity.ok("Book added to the library");
+        }
+    }
 }
