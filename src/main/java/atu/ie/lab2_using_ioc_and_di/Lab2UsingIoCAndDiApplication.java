ae8de4370c3064487b9905dcd9bf8eec115bb7c1
Mikaela-D <G00401392@atu.ie>
2023-10-10 15:40:32 +0100

    Created new project

---
+++ src/main/java/atu/ie/lab2_using_ioc_and_di/Lab2UsingIoCAndDiApplication.java
@@ -0,0 +1,13 @@
+package atu.ie.lab2_using_ioc_and_di;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab2UsingIoCAndDiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab2UsingIoCAndDiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/atu/ie/lab2_using_ioc_and_di/Lab2UsingIoCAndDiApplicationTests.java
@@ -0,0 +1,13 @@
+package atu.ie.lab2_using_ioc_and_di;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab2UsingIoCAndDiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
