c6bd25884a22125ccceb35157b82cce3812a8f94
Mikaela-D <G00401392@atu.ie>
2023-10-10 23:15:01 +0100

    Added code to RegisterUserBody

--- src/main/java/atu/ie/lab2_using_ioc_and_di/RequestBody/RegisterUserBody.java
+++ src/main/java/atu/ie/lab2_using_ioc_and_di/RequestBody/RegisterUserBody.java
@@ -1,5 +1,12 @@
 package atu.ie.lab2_using_ioc_and_di.RequestBody;
 
-public class RegisterUserBody {
+import lombok.Data;
+import org.springframework.stereotype.Service;
 
+@Data
+@Service
+public class RegisterUserBody {
+    public String msg(UserDetails userDetails){
+        return"Congratulations!! " +  userDetails.getName() + ". You are registered on this address: " + userDetails.getEmail();
+    }
 }
--- src/main/java/atu/ie/lab2_using_ioc_and_di/RequestBody/UserController.java
+++ src/main/java/atu/ie/lab2_using_ioc_and_di/RequestBody/UserController.java
@@ -1,4 +1,8 @@
 package atu.ie.lab2_using_ioc_and_di.RequestBody;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+
 }
