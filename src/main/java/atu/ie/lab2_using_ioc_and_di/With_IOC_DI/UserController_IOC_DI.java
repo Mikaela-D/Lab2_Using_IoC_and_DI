269337f3234dec52984b58d112098adbec4e7fff
Mikaela-D <G00401392@atu.ie>
2023-10-10 23:04:15 +0100

    Added @GetMapping code to UserController_IOC_DI

--- src/main/java/atu/ie/lab2_using_ioc_and_di/With_IOC_DI/UserController_IOC_DI.java
+++ src/main/java/atu/ie/lab2_using_ioc_and_di/With_IOC_DI/UserController_IOC_DI.java
@@ -1,6 +1,8 @@
 package atu.ie.lab2_using_ioc_and_di.With_IOC_DI;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,4 +14,9 @@ public class UserController_IOC_DI {
         this.userService = userService;
     }
 
+    @GetMapping("getUser1/{name}/{email}")
+    public String getUser(@PathVariable String name, @PathVariable String email){
+        userService.registerUser(name, email);
+        return name + "With IOC/DI registered email is :" + email;
+    }
 }
