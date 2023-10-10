ff0d61c8017b86a03ecc94602c869574c468f976
Mikaela-D <G00401392@atu.ie>
2023-10-10 23:01:39 +0100

    Added code to EmailService_IOC_DI

--- src/main/java/atu/ie/lab2_using_ioc_and_di/With_IOC_DI/EmailService_IOC_DI.java
+++ src/main/java/atu/ie/lab2_using_ioc_and_di/With_IOC_DI/EmailService_IOC_DI.java
@@ -1,4 +1,10 @@
 package atu.ie.lab2_using_ioc_and_di.With_IOC_DI;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class EmailService_IOC_DI {
+    public void sendEmail(String email, String message){
+        System.out.println("Sending email to " + email + ":" + message);
+    }
 }
